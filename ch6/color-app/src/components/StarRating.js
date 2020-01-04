1f770bf001d13396ba25852ed8db2d2a317fdb15
Mx. Kas Perch <the@nodebotani.st>
2020-01-04 14:45:29 -0600

    Working on state hooks

--- ch6/color-app/src/components/StarRating.js
+++ ch6/color-app/src/components/StarRating.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaStar} from 'react-icons/fa'
 
-const Star = ({selected = false}) => (
+const Star = ({selected = false, onSelect = f => f }) => (
   <FaStar color={selected ? 'red': 'grey'} />
 )
 
-const CreateArray = length => [...Array(length)]
+const createArray = length => [...Array(length)]
 
 export default function StarRating({totalStars = 5}) {
-  return CreateArray(totalStars).map((n, i) => <Star key={i} />)
+  const [selectedStars, setSelectedStars] = useState(0) 
+  return (
+    <>
+    {createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1) } />)}
+    <p>
+      {selectedStars} of {totalStars} stars
+    </p>
+    </>
+  )
 }
